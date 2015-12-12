27dd8a642d7f073fc9f554355ba9bead3a561477
Kalarrs Topham <k-dawg-klark@hotmail.com>
2015-12-12 13:41:24 -0700

    * Added ui-router

--- app/app.js
+++ app/app.js
@@ -1,39 +1,39 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  var app = angular.module('santasHelper', ['ui.router','firebase']);
-  
-  app.config(function routeConfig($stateProvider, $urlRouterProvider) {
+    var app = angular.module('santasHelper', ['ui.router', 'ui.bootstrap', 'firebase']);
 
-      $stateProvider
-          .state('main', {
-            abstract: true,
-            url: '/',
-            template: '<div ui-view></div>'
-          })
-          .state('ask', {
-            url: '',
-            parent: 'main',
-            templateUrl: 'app/ask/ask.html',
-            controller: 'AskController'
-          })
-          .state('santa', {
-            parent: 'main',
-            templateUrl: 'app/santa/santa.html',
-            controller: 'SantaController'
-          })
-      ;
+    app.config(function routeConfig($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/');
-  });
+        $stateProvider
+            .state('main', {
+                abstract: true,
+                url: '/',
+                template: '<div ui-view></div>'
+            })
+            .state('ask', {
+                url: '',
+                parent: 'main',
+                templateUrl: 'app/ask/ask.html',
+                controller: 'AskController'
+            })
+            .state('santa', {
+                parent: 'main',
+                templateUrl: 'app/santa/santa.html',
+                controller: 'SantaController'
+            })
+        ;
 
-  app.controller('HomeController', function($scope, $firebaseObject) {
-    var ref = new Firebase('https://santas-helper.firebaseio.com/data');
-    // download the data into a local object
-    var syncObject = $firebaseObject(ref);
-    // synchronize the object with a three-way data binding
-    // click on `index.html` above to see it used in the DOM!
-    syncObject.$bindTo($scope, 'data');
-  });
+        $urlRouterProvider.otherwise('/');
+    });
+
+    app.controller('HomeController', function ($scope, $firebaseObject) {
+        var ref = new Firebase('https://santas-helper.firebaseio.com/data');
+        // download the data into a local object
+        var syncObject = $firebaseObject(ref);
+        // synchronize the object with a three-way data binding
+        // click on `index.html` above to see it used in the DOM!
+        syncObject.$bindTo($scope, 'data');
+    });
 
 }());
