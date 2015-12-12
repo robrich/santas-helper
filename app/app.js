34281cc59cac8ab8c8df235ff49f474696e54c47
Rob Richardson <robrich@robrich.org>
2015-12-12 12:04:17 -0700

    Angular module and link to firebase

---
+++ app/app.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  var app = angular.module("santasHelper", ["firebase"]);
+  app.controller("HomeController", function($scope, $firebaseObject) {
+    var ref = new Firebase("https://santas-helper.firebaseio.com/data");
+    // download the data into a local object
+    var syncObject = $firebaseObject(ref);
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    syncObject.$bindTo($scope, "data");
+  });
+
+}());
