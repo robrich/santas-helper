1e39d89851df7c4a43138e28cd212011f6aa4d95
William <hello@bigwilliam.com>
2015-12-12 13:57:28 -0700

    added modal

--- app/ask/ask.js
+++ app/ask/ask.js
@@ -3,7 +3,20 @@
 
     var app = angular.module('santasHelper')
         .controller('AskController', function ($scope) {
+        	var modalInstance = $uibModal.open({
+			      // animation: $scope.animationsEnabled,
+			      templateUrl: 'app/ask/modal.html',
+			      controller: 'AskAcceptedController',
+			      size: 'sm'
+			    });
 
-        });
+			    modalInstance.result.finally(function (selectedItem) {
+			      $scope.selected = selectedItem;
+			    });
+        })
+        .controller('AskModalController', function ($scope) {
+
+        })
+        	
 
 }());
