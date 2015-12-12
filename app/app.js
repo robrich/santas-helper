9cbb652c5245a8bb118c2da399600ac1a54a172e
Kalarrs Topham <k-dawg-klark@hotmail.com>
2015-12-12 13:23:23 -0700

    * Added routes and pages

--- app/app.js
+++ app/app.js
@@ -1,7 +1,32 @@
 (function () {
   'use strict';
 
-  var app = angular.module('santasHelper', ['firebase']);
+  var app = angular.module('santasHelper', ['ui.router','firebase']);
+  
+  app.config(function routeConfig($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+          .state('main', {
+            abstract: true,
+            url: '/',
+            template: '<div ui-view></div>'
+          })
+          .state('ask', {
+            url: '',
+            parent: 'main',
+            templateUrl: 'app/ask/ask.html',
+            controller: 'AskController'
+          })
+          .state('santa', {
+            parent: 'main',
+            templateUrl: 'app/santa/santa.html',
+            controller: 'SantaController'
+          })
+      ;
+
+    $urlRouterProvider.otherwise('/');
+  });
+
   app.controller('HomeController', function($scope, $firebaseObject) {
     var ref = new Firebase('https://santas-helper.firebaseio.com/data');
     // download the data into a local object
